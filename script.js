23b92b8283d494a8b037b1634ca246a6e37d34a5
Tintuschris <tintuschriso@gmail.com>
2023-02-01 03:37:41 +0300

    running on sequelize and xampp

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#form');
+const input = document.querySelector('#input');
+const messagesList = document.querySelector('#messages');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const message = input.value;
+  input.value = '';
+
+  fetch('/message', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text: message }),
+  });
+});
+
+const loadMessages = () => {
+  fetch('/messages')
+    .then((response) => response.json())
+    .then((data) => {
+      messagesList.innerHTML = '';
+
+      data.messages.forEach((message) => {
+        const li = document.createElement('li');
+        li.innerHTML = message.text;
+        li.classList.add('message');
+        messagesList.appendChild(li);
+      });
+
+      setTimeout(loadMessages, 1000);
+    });
+};
+
+loadMessages();
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Chat app listening at http://localhost:${port}`);
+});
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('chatapp', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Message = sequelize.define('message', {
+  text: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+Message.sync({ force: true }).then(() => {
+  console.log('Message table created successfully.');
+});
+
+app.post('/message', (req, res) => {
+    Message.create({ text: req.body.text }).then((message) => {
+      console.log('Message created successfully:', message.text);
+      res.status(201).json({ message });
+    });
+  });
+
+  app.get('/messages', (req, res) => {
+    Message.findAll({ order: [['createdAt', 'DESC']], limit: 100 }).then((messages) => {
+      res.status(200).json({ messages });
+    });
+  });
+  
